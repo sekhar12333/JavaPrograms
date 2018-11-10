6596b04249345af47314a7cc4d8335c511d5d68f
sekhar12333 <44562807+sekhar12333@users.noreply.github.com>
2018-11-10 15:30:22 +0530

    Add files via upload

--- MethodOverLoading/src/Constructors.java
+++ MethodOverLoading/src/Constructors.java
@@ -2,11 +2,20 @@ public class Constructors {
 
 
     public static void main(String[] args) {
-        Bicycle b = new Bicycle(4,"Hero", 20);
-        System.out.println(b.gears);
-        System.out.println(b.name);
-        System.out.println(b.speed);
+        Bicycle yamha = new Bicycle(2, "Yamaha",30);
+        yamha.start();
+        Bicycle hero = new Bicycle(2, "Hero",40);
+        hero.start();
+        Bicycle avenger = new Bicycle(2, "avneger",40);
 
-        b.start();
+        Bicycle avenger1 = new Bicycle(2, "avneger",40);
+
+        Bicycle avenger3 = new Bicycle(2, "avneger",40);
+
+        Bicycle avenger2 = new Bicycle(2, "avneger",40);
+
+        System.out.println("Number of bicyles: "+Bicycle.numberOfBicycle);
+        System.out.println("tatal of wheels: "+Bicycle.totalWheels);
+
     }
 }
